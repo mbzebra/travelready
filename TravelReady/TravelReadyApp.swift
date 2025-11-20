d84d8c7ab1f2ce7fb5017701ecb79d6c3389385e
Marish <mbzebradev@gmail.com>
2025-11-19 21:27:06 -0700

    Initial Commit

---
+++ TravelReady/TravelReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelReadyApp.swift
+//  TravelReady
+//
+//  Created by Mariswaran Balasubramanian on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
